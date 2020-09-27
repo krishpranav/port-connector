349a89c1cf6a40f035e2a372e474dcd919ff452f
krishpranav <krisna.pranav@gmail.com>
2020-09-27 17:04:30 +0530

    Intial Commit

--- port-connecter.c
+++ port-connecter.c
@@ -1,5 +1,8 @@
+//a simple c tool for connecting to a port and recive the connection
+
+//imports
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -7,12 +10,13 @@
 #include <errno.h>
 #include <string.h>
 
+//sockaddr
 struct sockaddr_in dest;
 
-int main()
-{
+//main function
+int main(){
 	int msocket, con, port;
-	printf("Enter The Port To Connect");
+	printf("Enter The Port To Connect > ")
 	scanf("%d", &port);
 
 	if ((msocket = socket(AF_INET, SOCK_STREAM, 0)) == -1){
@@ -21,7 +25,7 @@ int main()
 	}
 	dest.sin_family = AF_INET;
 	dest.sin_port = htons(port);
-	bzero(dest.sin_zero, sizeof(dest.sin_zero));
+	bzero(dest.sin_zero, sizeof(dest.sin_zero))
 
 	if ((con = connect(msocket, (struct sockaddr *)&dest, sizeof(dest))) == -1){
 		perror("connect")
@@ -32,4 +36,6 @@ int main()
 		exit(1);
 	}
 	return 0;
+
 }
+	
